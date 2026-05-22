ef9d9274db4cf260903b48486df661e5a3e8c98d
leonelverde <verde.jara.leonel@gmail.com>
2026-05-22 12:03:56 -0500

    Tercer commit, MVC - Venta de entradas. Segunda adicion de exceptions a las clases.

--- src/main/java/modelo/Zona.java
+++ src/main/java/modelo/Zona.java
@@ -3,6 +3,8 @@ package modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import modelo.excepciones.CapacidadExcedidaException;
+import modelo.excepciones.EntradaNoDisponibleException;
 
 public class Zona{
     private String nombre;
@@ -17,9 +19,30 @@ public class Zona{
         this.entradas = new ArrayList<>();
     }
 
-    public boolean generarEntradas(){return true;}
+    public boolean generarEntradas(int cantidad) throws CapacidadExcedidaException {
+        if (entradas.size() + cantidad > capacidad) {
+            throw new CapacidadExcedidaException("No se pueden generar " + cantidad + " entradas. "
+                    + "La capacidad máxima de la zona " + nombre + " es " + capacidad + ".");
+    
+        }
+        for (int i = 0; i < cantidad; i++) {
+            entradas.add(new Entrada(entradas.size() + 1, "Disponible"));
+        }
+        return true;
+    }
 
     public Entrada[] mostrarEntrada(){return new Entrada[0];}
 
-    public Entrada[] venderEntrada(int numero){return new Entrada[0];}
+    public Entrada[] venderEntrada(int numero) throws CapacidadExcedidaException, EntradaNoDisponibleException{
+        int disponibles = 0;
+        for (Entrada e : entradas) {
+            disponibles++; 
+        }
+
+        if (numero > disponibles) {
+            throw new CapacidadExcedidaException("Solo quedan " + disponibles + " entradas disponibles en la zona " + nombre + ".");
+        }
+        
+        return new Entrada[numero];
+    }
 }
